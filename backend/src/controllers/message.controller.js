7015e5d4a9fd5f73710b67c0e6355dff8b168e3f
voravisha <voravisha2003@gmail.com>
2025-02-12 10:31:13 +0530

    Increase payload size limit and minor code formatting

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -44,7 +44,7 @@ export const sendMessage = async (req, res) => {
         let imageUrl
         if(image){
             const uploadResponse = await cloudinary.uploader.upload(image)
-            imageUrl = uploadResponse.secure_url
+            imageUrl = uploadResponse.secure_url;
         }
 
         const newMessage = new Message({
--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,8 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
